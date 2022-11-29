958730a40024edf4ca2658b2b70080ccaa09683d
brian <brian.ogango@student.moringaschool.com>
2022-11-29 08:20:55 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const text=document.getElementById('new-task-description');
+   const form = document.getElementById("create-task-form");
+  const submit = document.querySelector("input[type=submit]");
+  const ul = document.getElementById("tasks");
+  let totalList = [];
+
+   form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newLi = document.createElement("li"); //create li
+    const button = document.createElement("button"); //create remove  button
+    button.innerHTML = "remove"; //inner text 
+    button.addEventListener("click", () => {
+      //make button remove li
+      newLi.remove();
+    });
+    newLi.innerHTML = text.value; //li inner text
+    newLi.appendChild(button); //append remove button to li
+    ul.appendChild(newLi); //append li to ul
+    totalList.push(newLi); //push each li to array
+    console.log(totalList); //make sure they're added
+    text.value = ""; //erase value after its appended
+  });
 });
+
+
